e947551c617804269952b390cf648838938fdadb
javi279 <javixxherrera@gmail.com>
2024-10-02 13:11:27 -0600

    version 6

--- public/login.php
+++ public/login.php
@@ -28,16 +28,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if ($usuario) {
         // Iniciar sesión
         $_SESSION['user_id'] = $usuario['id'];
-        $_SESSION['user_name'] = $usuario['nombre']; // Suponiendo que tienes un campo 'nombre'
-        $_SESSION['user_role'] = $usuario['rol']; // Suponiendo que tienes un campo 'rol'
+        $_SESSION['user_name'] = $usuario['nombre'];
+        $_SESSION['user_role'] = $usuario['rol'];
 
         // Redirigir según el rol
         if ($usuario['rol'] === 'admin') {
             header('Location: ../views/admin/dashboard.php');
         } elseif ($usuario['rol'] === 'empleado') {
-            header('Location: ../views/empleado/dashboard.php'); // Crear un dashboard específico para empleados
+            header('Location: ../views/empleado/dashboard.php');
         } else {
-            // Redirigir a una página de error si el rol no está autorizado
             header('Location: ../public/login.php?error=rol_no_autorizado');
             exit;
         }
@@ -48,31 +47,40 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 ?>
 
-<!DOCTYPE html>
+<!doctype html>
 <html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="./dist/css/style.css" rel="stylesheet">
     <title>Iniciar Sesión</title>
-    <link rel="stylesheet" href="../public/styles/adminlte.min.css">
-</head>
-<body>
-    <div class="container">
-        <h1>Iniciar Sesión</h1>
-        <?php if (!empty($error)) { ?>
-            <div class="alert alert-danger"><?php echo htmlspecialchars($error); ?></div>
-        <?php } ?>
-        <form action="login.php" method="POST">
-            <div class="form-group">
-                <label for="email">Correo Electrónico</label>
-                <input type="email" class="form-control" id="email" name="email" required>
+  </head>
+  <body>
+    <div class="container-fluid">
+        <form action="login.php" method="POST" class="mx-auto">
+            <h4 class="text-center">Login</h4>
+            
+            <?php if (!empty($error)) { ?>
+                <div class="alert alert-danger text-center"><?php echo htmlspecialchars($error); ?></div>
+            <?php } ?>
+
+            <div class="mb-3 mt-5">
+              <label for="email" class="form-label">Correo Electrónico</label>
+              <input type="email" class="form-control" id="email" name="email" required>
             </div>
-            <div class="form-group">
-                <label for="password">Contraseña</label>
-                <input type="password" class="form-control" id="password" name="password" required>
+            <div class="mb-3">
+              <label for="password" class="form-label">Contraseña</label>
+              <input type="password" class="form-control" id="password" name="password" required>
             </div>
-            <button type="submit" class="btn btn-primary">Iniciar Sesión</button>
+            
+            <button type="submit" class="btn btn-primary mt-5">Iniciar Sesión</button>
         </form>
     </div>
-</body>
+
+    <!-- Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
 </html>
